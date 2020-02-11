a169b8f56efdf8568a38d6d1fb154feb00f0f5bb
ceosss <sswarajsamant@gmail.com>
2020-02-11 13:46:59 +0530

    fixed bugs

--- export.js
+++ export.js
@@ -1,6 +1,8 @@
 const connectd = {
-  CLIENTID =
+  clientID:
     "105475092538-qq100ud277gucas8pagjucb8ookd42qh.apps.googleusercontent.com",
-  CLIENTSECRET = "i3Q1ZIa_wC1pmO0xf-EFkYYl",
-  CALLBACKURL = "https://my-weather-app-v1.herokuapp.com/whatever"
-}
+  clientSecret: "i3Q1ZIa_wC1pmO0xf-EFkYYl",
+  callbackURL: "https://my-weather-app-v1.herokuapp.com/whatever"
+};
+
+exports.connectd = connectd;
